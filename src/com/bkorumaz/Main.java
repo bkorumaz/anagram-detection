4fcd5673b830f81fd71240ec1f901febca670443
bkorumaz <bahadir.korumaz@gmail.com>
2020-06-10 22:29:36 +0300

    getting array of strings from two lines

--- src/com/bkorumaz/Main.java
+++ src/com/bkorumaz/Main.java
@@ -1,8 +1,34 @@
 package com.bkorumaz;
 
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            File file = new File("anagrams.txt");
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line;
+            line = br.readLine();
+            List<String> items1 = Arrays.asList(line.split("\\s*,\\s*"));
+            line = br.readLine();
+            List<String> items2 = Arrays.asList(line.split("\\s*,\\s*"));
+
+            System.out.println("first group of items" + items1.toString());
+            System.out.println("second group of items" + items2.toString());
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 }
